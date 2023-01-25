b9df3381a09a5574c0179ae4fa25ebc438e7c026
Xavier <73123181+Xavier2414@users.noreply.github.com>
2023-01-25 16:30:06 +0100

    estilos basicos

---
+++ lib/estilos.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme = ThemeData.light().copyWith(
+
+    backgroundColor: Color.fromRGBO(228,229,234,1.000),
+    primaryColor: Color.fromRGBO(22,53,77,1.000),
+    scaffoldBackgroundColor: Color.fromRGBO(228,229,234,1.000),
+
+
+
+
+
+
+);
+
