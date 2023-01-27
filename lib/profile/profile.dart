13a1ec94e0d4d29ba089ee85fe43b14d0dc9d414
Carlos Sallán <113118603+CarlosSallan@users.noreply.github.com>
2023-01-27 16:18:30 +0100

    perfil

--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -15,6 +15,77 @@ class _profileState extends State<profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body:
+      Row(
+        children: [
+          Expanded(child:
+          Column(
+
+          ),
+          ),
+          Expanded(
+            child:
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Perfil"),
+                Container(
+                  height:175,
+                  width: 175,
+                  decoration: BoxDecoration(
+                      color: Colors.blueGrey,
+                      borderRadius: BorderRadius.circular(100)
+                    //more than 50% of width makes circle
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.all(Radius.circular(50))),
+                  child: TextButton(
+                    child: Text(
+                      "Cambiar avatar",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () {
+                      //añadir onpressed
+                    },
+                  ),
+                ),
+                const TextField(
+                  obscureText: false,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Nombre de usuario',
+                  ),
+                ),
+                const TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Contraseña',
+                  ),
+                ),
+                TextButton(
+                    child: Text('Cambiar.', style: TextStyle(fontSize: 20.0),),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => main_screen()),
+                      );
+                    }
+                ),
+              ],
+            ),
+          ),
+          Expanded(child:
+          Column(
+
+          ),
+          ),
+        ],
+      ),
       bottomNavigationBar: Container(
         color: Color.fromRGBO(22,53,77,1.000) ,
         padding: EdgeInsets.all(10),
@@ -68,3 +139,5 @@ class _profileState extends State<profile> {
     );
   }
 }
+
+
