8d3d0e125696f213bbb998a429f3e440de938ba8
Xavier <73123181+Xavier2414@users.noreply.github.com>
2023-01-27 16:32:09 +0100

    menu levitante

---
+++ lib/container/menu.dart
@@ -0,0 +1,88 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import '../login/login.dart';
+import '../profile/profile.dart';
+
+
+class menu extends StatefulWidget {
+  const menu({Key? key}) : super(key: key);
+
+  @override
+  State<menu> createState() => _menuState();
+}
+
+class _menuState extends State<menu> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: Container(
+        margin: const EdgeInsets.symmetric(
+            vertical: 15.0,
+            horizontal: 15.0
+        ),
+        decoration:
+        BoxDecoration(
+            color: Color.fromRGBO(22,53,77,1.000),
+            borderRadius: BorderRadius.circular(100)
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 15.0,
+            vertical:  10.0,
+
+          ),
+
+          child: GNav(
+
+            backgroundColor: Color.fromRGBO(22,53,77,1.000),
+            activeColor: Colors.white60,
+            color: Colors.white,
+            padding: EdgeInsets.all(10),
+            gap: 20,
+            onTabChange: (index){
+              print(index);
+            },
+            tabs: [
+              GButton(
+                  icon: Icons.map,
+
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => login()),
+                    );
+                  }
+              ),
+              GButton(
+                icon: Icons.chat,
+
+              ),
+              GButton(
+                icon: Icons.sos,
+
+              ),
+              GButton(
+                icon: Icons.settings,
+
+              ),
+              GButton(
+                  icon: IconData(0xf522, fontFamily: 'MaterialIcons'),
+
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => profile()),
+                    );
+                  }
+              ),
+
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main_screen/main_screen.dart
+++ lib/main_screen/main_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import '../login/login.dart';
 import '../profile/profile.dart';
+import '../container/menu.dart';
 
 class main_screen extends StatefulWidget {
   static const String id = 'main_screen';
@@ -17,61 +18,99 @@ class _main_screenState extends State<main_screen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: Container(
-        color: Color.fromRGBO(22,53,77,1.000) ,
-        padding: EdgeInsets.all(10),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 15.0,
-            vertical:  20.0,
-          ),
-          child: GNav(
-            backgroundColor: Color.fromRGBO(22,53,77,1.000),
-            activeColor: Colors.white60,
-            color: Colors.white,
-           padding: EdgeInsets.all(10),
-            gap: 20,
-            onTabChange: (index){
-              print(index);
-            },
-            tabs: [
-              GButton(
-                  icon: Icons.logout,
-                      text:'Inicio',
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => login()),
-                  );
-                }
-              ),
-              GButton(
-                  icon: Icons.chat,
-                text:'Like',
-              ),
-              GButton(
-                  icon: Icons.sos,
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
 
+          children: [
+            Container(
+
+              child: Text("SOS"),
+              margin : EdgeInsets.fromLTRB(20, 8, 8, 16),
+              width: 150,
+              height: 150,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                color: Colors.black12,
               ),
-              GButton(
-                icon: Icons.settings,
-                text:'Buscar',
-              ),
-              GButton(
-                  icon: IconData(0xf522, fontFamily: 'MaterialIcons'),
-                text:'Usuario',
+
+            ),
+          ],
+
+
+          ),
+
+
+        ],
+      ),
+
+
+      bottomNavigationBar: Container(
+        margin: const EdgeInsets.symmetric(
+          vertical: 15.0,
+          horizontal: 15.0
+        ),
+        decoration:
+        BoxDecoration(
+            color: Color.fromRGBO(22,53,77,1.000),
+            borderRadius: BorderRadius.circular(100)
+        ),
+        child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 15.0,
+              vertical:  10.0,
+
+            ),
+
+            child: GNav(
+
+              backgroundColor: Color.fromRGBO(22,53,77,1.000),
+              activeColor: Colors.white60,
+              color: Colors.white,
+             padding: EdgeInsets.all(10),
+              gap: 20,
+              onTabChange: (index){
+                print(index);
+              },
+              tabs: [
+                GButton(
+                    icon: Icons.map,
+
                   onPressed: () {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => profile()),
+                      MaterialPageRoute(builder: (context) => login()),
                     );
                   }
-              ),
+                ),
+                GButton(
+                    icon: Icons.chat,
+
+                ),
+                GButton(
+                    icon: Icons.sos,
+
+                ),
+                GButton(
+                  icon: Icons.settings,
+
+                ),
+                GButton(
+                    icon: IconData(0xf522, fontFamily: 'MaterialIcons'),
+
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => profile()),
+                      );
+                    }
+                ),
 
 
-            ],
+              ],
+            ),
           ),
-        ),
       ),
     );
   }
