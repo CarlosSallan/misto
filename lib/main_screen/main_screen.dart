d929bc684a8511e21cfaea5267a932a4a52dcf49
Carlos Sallán <113118603+CarlosSallan@users.noreply.github.com>
2023-02-02 18:20:11 +0100

    asd

--- lib/main_screen/main_screen.dart
+++ lib/main_screen/main_screen.dart
@@ -1,11 +1,13 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import '../login/login.dart';
 import '../profile/profile.dart';
 import '../profile/profile2.dart';
 import '../container/menu.dart';
+import '../container/botonHold.dart';
 
 class main_screen extends StatefulWidget {
   static const String id = 'main_screen';
@@ -21,29 +23,39 @@ class _main_screenState extends State<main_screen> {
     return Scaffold(
       body: Column(
         children: [
-          Expanded(child:
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-
-            children: [
-              Container(
-
-                child: Text("SOS"),
-                margin : EdgeInsets.fromLTRB(20, 8, 8, 16),
-                width: 150,
-                height: 150,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(100),
-                  color: Colors.black12,
-                ),
-
-              ),
-            ],
-
-
+          //Mapa
+          Expanded(
+            child:
+              Row(
+                children: [
+                  Expanded(
+                      child:
+                      Image(image: NetworkImage('https://static4.depositphotos.com/1000263/340/v/450/depositphotos_3408227-stock-illustration-world-map-blue.jpg'))
+                  )
+                ],
+              )
           ),
+          //Deslizar amigos
+          Container(
+            color: Colors.amber,
+            height: MediaQuery.of(context).size.height * 0.20,
+            width: MediaQuery.of(context).size.width,
+            child:
+              Row()
           ),
+          //Boton SOS
+          Container(
+            height: MediaQuery.of(context).size.height * 0.20,
+            width: MediaQuery.of(context).size.width,
+            child:
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
 
+              ],
+            )
+          ),
+          //Menu
           Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -55,8 +67,6 @@ class _main_screenState extends State<main_screen> {
                 )
               ]
           )
-
-
         ],
       ),
     );
