b8ceec4ab4b13c0098326ab117a0bc12bae57bf3
sofiyakogut <113117982+sofiyakogut@users.noreply.github.com>
2023-03-15 16:52:43 +0100

    map location ✌

--- lib/main_screen/main_screen.dart
+++ lib/main_screen/main_screen.dart
@@ -2,10 +2,19 @@ import 'dart:async';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:misto/main_screen/mymap.dart';
 import '../container/menu.dart';
 import '../container/ripple_button.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:location/location.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:location/location.dart' as loc;
+import 'package:permission_handler/permission_handler.dart';
 
 class main_screen extends StatefulWidget {
   static const String id = 'main_screen';
@@ -20,31 +29,77 @@ class _main_screenState extends State<main_screen> {
 
   static const LatLng sourceLocation = LatLng(37.33500926, -122.03272188);
   static const LatLng destination = LatLng(37.33429383, -122.06600055);
+  final loc.Location location = loc.Location();
+  StreamSubscription<loc.LocationData>? _locationSubscription;
 
+  @override
+  void initState() {
+    super.initState();
+    _requestPermission();
+    location.changeSettings(interval: 300, accuracy: loc.LocationAccuracy.high);
+    location.enableBackgroundMode(enable: true);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(228,229,234,1.000),
       body: Column(
         children: [
-          //Mapa
+          TextButton(
+              onPressed: () {
+                _getLocation();
+              },
+              child: Text('add my location')),
+          TextButton(
+              onPressed: () {
+                _listenLocation();
+              },
+              child: Text('enable live location')),
+          TextButton(
+              onPressed: () {
+                _stopListening();
+              },
+              child: Text('stop live location')),
           Expanded(
-            child:
-              Row(
-                children: [
-                  Expanded(
-                      child: GoogleMap(
-                        initialCameraPosition: CameraPosition(target: sourceLocation, zoom: 14.5),
-
-                      ),
-
-                  )
-                ],
-              )
-          ),
+              child: StreamBuilder(
+                stream:
+                FirebaseFirestore.instance.collection('location').snapshots(),
+                builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (!snapshot.hasData) {
+                    return Center(child: CircularProgressIndicator());
+                  }
+                  return ListView.builder(
+                      itemCount: snapshot.data?.docs.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title:
+                          Text(snapshot.data!.docs[index]['name'].toString()),
+                          subtitle: Row(
+                            children: [
+                              Text(snapshot.data!.docs[index]['latitude']
+                                  .toString()),
+                              SizedBox(
+                                width: 20,
+                              ),
+                              Text(snapshot.data!.docs[index]['longitude']
+                                  .toString()),
+                            ],
+                          ),
+                          trailing: IconButton(
+                            icon: Icon(Icons.directions),
+                            onPressed: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) =>
+                                      MyMap(snapshot.data!.docs[index].id)));
+                            },
+                          ),
+                        );
+                      });
+                },
+              )),
           //Deslizar amigos
           Container(
-            color: Colors.amber,
+            color: Colors.deepPurpleAccent,
             height: MediaQuery.of(context).size.height * 0.20,
             width: MediaQuery.of(context).size.width,
             child:
@@ -78,4 +133,51 @@ class _main_screenState extends State<main_screen> {
       ),
     );
   }
+  _getLocation() async {
+    try {
+      final loc.LocationData _locationResult = await location.getLocation();
+      await FirebaseFirestore.instance.collection('location').doc('user1').set({
+        'latitude': _locationResult.latitude,
+        'longitude': _locationResult.longitude,
+        'name': 'john'
+      }, SetOptions(merge: true));
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> _listenLocation() async {
+    _locationSubscription = location.onLocationChanged.handleError((onError) {
+      print(onError);
+      _locationSubscription?.cancel();
+      setState(() {
+        _locationSubscription = null;
+      });
+    }).listen((loc.LocationData currentlocation) async {
+      await FirebaseFirestore.instance.collection('location').doc('user1').set({
+        'latitude': currentlocation.latitude,
+        'longitude': currentlocation.longitude,
+        'name': 'john'
+      }, SetOptions(merge: true));
+    });
+  }
+
+  _stopListening() {
+    _locationSubscription?.cancel();
+    setState(() {
+      _locationSubscription = null;
+    });
+  }
+
+  _requestPermission() async {
+    var status = await Permission.location.request();
+    if (status.isGranted) {
+      print('done');
+    } else if (status.isDenied) {
+      _requestPermission();
+    } else if (status.isPermanentlyDenied) {
+      openAppSettings();
+    }
+  }
 }
+
