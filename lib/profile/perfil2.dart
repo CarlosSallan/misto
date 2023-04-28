e10603f4d0aa1597affd2cfb133a0d2b509def47
sofiyakogut <113117982+sofiyakogut@users.noreply.github.com>
2023-04-28 17:21:14 +0200

    avatar, storage y unas mierdas  mas

--- lib/profile/perfil2.dart
+++ lib/profile/perfil2.dart
@@ -1,15 +1,20 @@
-
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 import 'package:flutter/rendering.dart';
 import 'package:misto/main_screen/main_screen.dart';
 import '../Amigos/aceptarUsers.dart';
 import '../user.dart';
-import '../Amigos/seguirUsers.dart';
-import '../acceder/login.dart';
-import 'UserDetailScreen.dart';
+import 'package:path/path.dart' as Path;
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as Path;
+
 
 
 class perfil2 extends StatefulWidget {
@@ -21,6 +26,57 @@ class perfil2 extends StatefulWidget {
 }
 
 class _perfil2State extends State<perfil2> {
+
+  final ImagePicker _picker = ImagePicker();
+  String? _uploadedFileURL;
+
+  Future chooseFile() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _uploadedFileURL = null;
+      });
+      uploadFile(File(pickedFile.path));
+    } else {
+      print('No image selected');
+    }
+  }
+
+  Future captureAndUploadImage() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.camera);
+
+    if (pickedFile != null) {
+      setState(() {
+        _uploadedFileURL = null;
+      });
+      uploadFile(File(pickedFile.path));
+    } else {
+      print('No image captured');
+    }
+  }
+
+  Future uploadFile(File file) async {
+    String fileName = '${Path.basename(file.path)}';
+    Reference storageReference = FirebaseStorage.instance.ref().child('avatars/$fileName');
+    UploadTask uploadTask = storageReference.putFile(file);
+
+    await uploadTask.whenComplete(() async {
+      print('File Uploaded');
+
+      await storageReference.getDownloadURL().then((fileURL) {
+        setState(() {
+          _uploadedFileURL = fileURL;
+          updateAvatarURL(fileURL);
+        });
+      });
+    });
+  }
+
+  void updateAvatarURL(String fileURL) async {
+    await FirebaseFirestore.instance.collection('Users').doc(user?.uid).update({'Avatar': fileURL});
+  }
+
 
   late Stream<DocumentSnapshot> _userStream;
   User? user = FirebaseAuth.instance.currentUser;
@@ -93,7 +149,7 @@ class _perfil2State extends State<perfil2> {
               ),
             ),
             Positioned(
-              top: 210.0,
+              top: 350.0,
               right: 30.0,
               child: Material(
                 color:  Color.fromRGBO(228,229,234,1.000),
@@ -122,6 +178,41 @@ class _perfil2State extends State<perfil2> {
                   iconSize: 48.0, // Ajusta el tamaño del botón aquí
                   padding: EdgeInsets.all(8.0), // Ajusta el padding para aumentar el área de toque del botón
                 ),
+              ),
+            ),
+
+            Positioned(
+              left:0,
+              top:0,
+              bottom: 0,
+              right: 0,
+              child: _uploadedFileURL != null
+                  ? CircleAvatar(
+                backgroundImage: NetworkImage(_uploadedFileURL!),
+                radius: 80,
+              )
+                  : CircleAvatar(
+                backgroundImage: AssetImage('assets/avatar_prueba.jpg'), // Asegúrate de tener una imagen predeterminada en tu carpeta 'assets/images'
+                radius: 80,
+              ),
+            ),
+            Positioned(
+              top: 320,
+              right: 100,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: chooseFile,
+                    icon: Icon(Icons.photo_library),
+                    label: Text('Galería'),
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: captureAndUploadImage,
+                    icon: Icon(Icons.camera),
+                    label: Text('Cámara'),
+                  ),
+                ],
               ),
             ),
 
