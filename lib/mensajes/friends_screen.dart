66a4a523a3af109330e138b10219776f3c73e62b
sofiyakogut <113117982+sofiyakogut@users.noreply.github.com>
2023-05-16 20:13:52 +0200

    chat

--- lib/mensajes/friends_screen.dart
+++ lib/mensajes/friends_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:misto/main_screen/main_screen.dart';
@@ -10,7 +11,8 @@ import 'package:cached_network_image/cached_network_image.dart';
 
 class FriendsScreen extends StatelessWidget {
   final Usuario currentUser;
-
+  final User? user = FirebaseAuth.instance.currentUser;
+  late Stream<DocumentSnapshot> _userStream;
   FriendsScreen({
     required this.currentUser,
   });
@@ -18,74 +20,117 @@ class FriendsScreen extends StatelessWidget {
   final Stream<QuerySnapshot> _friendsStream =
   FirebaseFirestore.instance.collection('Users').snapshots();
 
+
+
   @override
   Widget build(BuildContext context) {
+    _userStream = FirebaseFirestore.instance
+        .collection('Users')
+        .doc(currentUser.id)
+        .snapshots();
     return Scaffold(
-      backgroundColor: Color.fromRGBO(22, 53, 77, 1.000),
-      body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                _top(),
-                Spacer(),
-                Padding(
-                  padding: EdgeInsets.only(right: 30.0),
-                  child: Material(
-                    color: Colors.white.withOpacity(0.5),
-                    borderRadius: BorderRadius.circular(10.0),
-                    child: IconButton(
-                      icon: Icon(
-                        Icons.home,
-                        size: 36.0,
-                        color: Color.fromRGBO(22, 53, 77, 1.000),
-                      ),
-                      onPressed: () => Navigator.push(
-                          context,
-                          new MaterialPageRoute(
-                              builder: (context) => main_screen(currentUser: currentUser))),
-                      iconSize: 48.0,
-                      padding: EdgeInsets.all(8.0),
-                    ),
-                  ),
-                ),
-              ],
-            ),
+        body: StreamBuilder<DocumentSnapshot>(
+            stream: _userStream,
+            builder:
+                (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+                  if (!snapshot.hasData) {
+                    return Center(child: CircularProgressIndicator());
+                  }
+                  Map<String, dynamic>? data =
+                  snapshot.data?.data() as Map<String, dynamic>?;
 
-            _body(),
-          ],
-        ),
-      ),
+                  return Scaffold(
+                    backgroundColor: Color.fromRGBO(22, 53, 77, 1.000),
+                    body: SafeArea(
+                      child: Column(
+
+                        children: [
+                          Row(
+                            children: [
+                              _top(context),
+                              Spacer(),
+                              Padding(
+                                padding: EdgeInsets.only(right: 30.0),
+                                child: Material(
+                                  color: Colors.white.withOpacity(0.5),
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  child: ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                      primary: Color.fromRGBO(22,53,77,1.000),
+                                    ),
+                                    onPressed: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(builder: (context) => perfil2()),
+                                      );
+                                    },
+                                    child: ClipOval(
+
+                                      child: data?['Avatar'] != null
+                                          ? CircleAvatar(
+                                        backgroundImage: NetworkImage(
+                                            data?['Avatar']),
+                                        radius: 50,
+                                      )
+                                          : CircleAvatar(
+                                        backgroundImage:
+                                        AssetImage('assets/avatar_prueba.jpg'),
+                                        radius: 50,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+
+                          _body(),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+    ),
     );
   }
 
 
-
-  Widget _top() {
+  Widget _top(BuildContext context) {
     return Container(
       padding: EdgeInsets.only(left: 30),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          SizedBox(height: 20),
-          Text(
-            'Habla con \ntus amigos',
-            style: TextStyle(
-                fontSize: 28, fontWeight: FontWeight.bold, color: Colors.white),
-          ),
-          SizedBox(height: 20),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.02,),
           Row(
             children: [
-              SizedBox(
-                width: 55,
+                Material(
+                  color: Colors.white.withOpacity(0.5),
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: IconButton(
+                    icon: Icon(
+                      Icons.arrow_back_ios_sharp,
+                      size: 36.0,
+                      color: Color.fromRGBO(22, 53, 77, 1.000),
+                    ),
+                  onPressed: () => Navigator.push(
+                      context,
+                      new MaterialPageRoute(
+                          builder: (context) => main_screen(currentUser: currentUser))),
+                  iconSize: 48.0,
+                  padding: EdgeInsets.all(8.0),
+                ),
               ),
-
+              SizedBox(width: MediaQuery.of(context).size.width * 0.02,),
+              Text(
+                'Habla con \ntus amigoss',
+                style: TextStyle(
+                    fontSize: 28, fontWeight: FontWeight.bold, color: Colors.white),
+              ),
             ],
           ),
-
-
+          SizedBox(height: MediaQuery.of(context).size.height * 0.02,),
         ],
-
 
       ),
     );
@@ -113,6 +158,8 @@ class FriendsScreen extends StatelessWidget {
               return Center(child: CircularProgressIndicator());
             }
 
+
+
             // Filtrar al usuario actual de la lista de amigos
             final friends = snapshot.data!.docs
                 .where((userDoc) => userDoc.id != currentUser.id);
@@ -134,17 +181,31 @@ class FriendsScreen extends StatelessWidget {
                           title: Text('Error: ${lastMessageSnapshot.error}'));
                     }
 
-                    if (lastMessageSnapshot.connectionState ==
-                        ConnectionState.waiting) {
-                      return ListTile(title: Text('Cargando...'));
+                    String? lastMessage;
+                    if (lastMessageSnapshot.data != null && lastMessageSnapshot.data!['text'] != null) {
+                      lastMessage = lastMessageSnapshot.data?['text'];
                     }
 
-                    String? lastMessage = lastMessageSnapshot.data?['text'];
-                    DateTime? lastMessageTime = (lastMessageSnapshot.data?['timestamp']
-                    as Timestamp?)
-                        ?.toDate();
+                    DateTime? lastMessageTime;
+                    if (lastMessageSnapshot.data != null && lastMessageSnapshot.data!['timestamp'] != null) {
+                      lastMessageTime = (lastMessageSnapshot.data?['timestamp']
+                      as Timestamp?)
+                          ?.toDate();
+                    }
 
                     DocumentSnapshot user = snapshot.data!.docs[index];
+                    Map<String, dynamic>? userData = user.data() as Map<String, dynamic>?;
+                    String? avatarUrl;
+                    if (userData != null && userData['Avatar'] != null) {
+                      avatarUrl = userData['Avatar'];
+                    }
+
+                    DocumentSnapshot friendDoc = snapshot.data!.docs[index];
+                    Map<String, dynamic>? friendData = friendDoc.data() as Map<String, dynamic>?;
+                    String? fullName;
+                    if (friendData != null && friendData['FullName'] != null) {
+                      fullName = friendData['FullName'];
+                    }
 
                     return Column(
                       children: [
@@ -154,15 +215,17 @@ class FriendsScreen extends StatelessWidget {
                             horizontal: 5.0,
                             vertical: 15.0,
                           ),
-                          leading: ClipOval(
+                          leading: avatarUrl != null
+                              ? ClipOval(
                             child: Image.network(
-                              user['Avatar'],
+                              avatarUrl,
                               width: MediaQuery.of(context).size.width * 0.07,
                               height: 100,
                               fit: BoxFit.cover,
                             ),
-                          ),
-                          title: Text(friendDoc['FullName']),
+                          )
+                              : SizedBox.shrink(), // Add some placeholder widget here
+                          title: fullName != null ? Text(fullName) : Text('Unknown user'),
                           subtitle: lastMessage == null
                               ? Text('No hay mensajes')
                               : Text(
@@ -188,33 +251,11 @@ class FriendsScreen extends StatelessWidget {
                     );
                   },
                 );
+
+
               },
             );
           },
-        ),
-      ),
-    );
-  }
-}
-
-
-class Avatar extends StatelessWidget {
-  final double size;
-  final image;
-  final EdgeInsets margin;
-  Avatar({this.image, this.size = 50, this.margin = const EdgeInsets.all(0)});
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: margin,
-      child: Container(
-        width: size,
-        height: size,
-        decoration: new BoxDecoration(
-          shape: BoxShape.circle,
-          image: new DecorationImage(
-            image: AssetImage(image),
-          ),
         ),
       ),
     );
